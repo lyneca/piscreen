4582d6c1139ddca518b964495e81ed4fadc3218a
lyneca <lukemtuthill@gmail.com>
2019-12-16 20:23:16 +1100

    Remove unused imports and functions

--- src/views/file.rs
+++ src/views/file.rs
@@ -3,14 +3,6 @@ extern crate embedded_graphics;
 use std::fs::DirEntry;
 use std::path::Path;
 
-use embedded_graphics::fonts::{Font,Font6x8};
-use embedded_graphics::coord::Coord;
-use embedded_graphics::prelude::*;
-// use embedded_graphics::image::Image1BPP;
-use embedded_graphics::pixelcolor::PixelColorU8;
-use embedded_graphics::primitives::{Rect, Line};
-use embedded_graphics::Drawing;
-
 use crate::{
     View, ReturnState, Display,
     buttons::ButtonSet,
@@ -33,26 +25,14 @@ impl<'a> FileView<'a> {
         }
     }
 
-    /// Draw the directory name on the top of the screen.
-    fn draw_dirname(&self, disp: &mut Display) {
-        let dirname = self.path.file_name().unwrap().to_str().unwrap();
-        disp.draw(
-            Rect::new(Coord::new(0, 0), Coord::new(10, 127))
-            .with_stroke(Some(PixelColorU8(0)))
-            .with_fill(Some(PixelColorU8(0)))
-            .into_iter()
-        );
-        disp.draw(Font6x8::render_str(dirname).into_iter());
-        disp.draw(
-            Line::new(Coord::new(0, 10), Coord::new(127, 10))
-            .with_stroke(Some(PixelColorU8(1)))
-            .into_iter());
+    pub fn get_name(&self) -> String {
+        self.path.file_name().unwrap().to_str().unwrap().to_owned()
     }
 }
 
 impl<'a> View for FileView<'a> {
     fn render(&mut self, disp: &mut Display) {
-        self.menu.set_name(self.path.file_name().unwrap().to_str().unwrap());
+        self.menu.set_name(self.get_name().as_ref());
         self.menu.set_entries(
             self.path.read_dir().unwrap().map(|dir: Result<DirEntry, std::io::Error>| -> MenuEntry {
                 let name = dir.unwrap().file_name().to_str().unwrap().to_owned();
